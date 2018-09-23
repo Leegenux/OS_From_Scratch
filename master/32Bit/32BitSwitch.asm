7e21f6e4205d5aeed6efc772bd7fe18e1f42604a
limian <qq1350747952@163.com>
2018-09-23 17:16:31 +0800

    Update comments

--- master/32Bit/32BitSwitch.asm
+++ master/32Bit/32BitSwitch.asm
@@ -1,25 +1,25 @@
 [bits 16]
 switch_to_pm:   ; Protected mode
-    cli         ; Disable interrupt
-    lgdt [gdt_descriptor]   ; Load the GDT descriptor
+    cli         ; 1. Disable incoming tinterrupt
+    lgdt [gdt_descriptor]   ; 2. Load the GDT descriptor
 
-    mov eax, cr0            ; Set the least bit(32-bit mode flag bit) to 1
+    mov eax, cr0            ; 3. Set the least bit(32-bit mode flag bit) to 1
     or eax, 0x1
     mov cr0, eax
 
     ; Current stage: the segmentation approach is changed. CODE_SEG is just a offset to the GDT_START
-    jmp CODE_SEG:init_pm    ; Far jump by using a different segment
+    jmp CODE_SEG:init_pm    ; 4. Far jump by using a different segment to flush the pipeline
 
 [bits 32]
 init_pm:       
-    mov ax, DATA_SEG       ; Update the segment registers
+    mov ax, DATA_SEG       ; 5. Update the segment registers
     mov ds, ax
     mov ss, ax
     mov es, ax
     mov fs, ax             ; Following two are OS-specific registers
     mov gs, ax
 
-    mov ebp, 0x90000        ; Update the stack registers
+    mov ebp, 0x90000       ; Update the stack registers
     mov esp, ebp
 
     call BEGIN_PM
