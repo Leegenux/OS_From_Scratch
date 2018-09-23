13075ef90cbb0fba6ecc5af4c319e8e7e5a2edff
limian <qq1350747952@163.com>
2018-09-23 17:59:22 +0800

    Add 16-bit print code

---
+++ master/16BitPrint/16BitPrint.asm
@@ -0,0 +1,62 @@
+[org 0x7c00]
+; [bits 16] is not must, cuz this function can be used both in 16-bit and 32-bit
+
+print_16_string:    ; Given that the address has been stored in bx
+    push ax
+    push bx
+    mov ah, 0x0e    ; TTY
+    
+    print_16_string_loop:
+        mov al, [bx]
+        
+        cmp al, 0x0e  ; Jump handling
+        je print_16_string_done
+
+        int 0x10
+        inc bx
+        jmp print_16_string_loop
+
+    print_16_string_done:
+        push bx
+        push ax
+        ret
+
+print_16_hex:
+    pusha
+
+    mov ah, 0x0e
+    mov cx, 0x4
+
+    mov al, "0"     ; Prefix
+    int 0x10
+    mov al, "x"
+    int 0x10
+
+    print_16_hex_loop:
+        dec cx          ; Decrement the loop count
+
+        mov al, 0x0f    ; Rotate left and get the least four bits of bx
+        rol bx, 4
+        and al, bl
+
+        cmp al, 0x0a ; Decode using ASCII code table
+        jl less_than_ten
+
+        greater_than_ten:
+            add al, "a" - 0x0a
+            jmp done_decode
+    
+        less_than_ten:
+            add al, "0"
+
+        done_decode:
+            int 0x10
+        
+            cmp cx, 0x0
+            jne print_16_hex_loop
+
+    popa
+    ret
+
+    
+
