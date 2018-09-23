67c34e4467a8a39e20bc5eed30ad2e2913e15e96
limian <qq1350747952@163.com>
2018-09-23 18:21:47 +0800

    Done writing 16 bit print and testing

--- master/16BitPrint/16BitPrint.asm
+++ master/16BitPrint/16BitPrint.asm
@@ -7,19 +7,19 @@ print_16_string:    ; Given that the address has been stored in bx
     mov ah, 0x0e    ; TTY
     
     print_16_string_loop:
-        mov al, [bx]
-        
-        cmp al, 0x0e  ; Jump handling
-        je print_16_string_done
+    mov al, [bx]
+    
+    cmp al, 0x0  ; Jump handling
+    je print_16_string_done
 
-        int 0x10
-        inc bx
-        jmp print_16_string_loop
+    int 0x10
+    inc bx
+    jmp print_16_string_loop
 
     print_16_string_done:
-        push bx
-        push ax
-        ret
+    pop bx
+    pop ax
+    ret
 
 print_16_hex:
     pusha
@@ -33,30 +33,41 @@ print_16_hex:
     int 0x10
 
     print_16_hex_loop:
-        dec cx          ; Decrement the loop count
+    dec cx          ; Decrement the loop count
 
-        mov al, 0x0f    ; Rotate left and get the least four bits of bx
-        rol bx, 4
-        and al, bl
+    mov al, 0x0f    ; Rotate left and get the least four bits of bx
+    rol bx, 4
+    and al, bl
 
-        cmp al, 0x0a ; Decode using ASCII code table
-        jl less_than_ten
+    cmp al, 0x0a ; Decode using ASCII code table
+    jl less_than_ten
 
-        greater_than_ten:
-            add al, "a" - 0x0a
-            jmp done_decode
-    
-        less_than_ten:
-            add al, "0"
+    greater_than_ten:
+    add al, "a" - 0x0a
+    jmp done_decode
 
-        done_decode:
-            int 0x10
-        
-            cmp cx, 0x0
-            jne print_16_hex_loop
+    less_than_ten:
+    add al, "0"
+
+    done_decode:
+    int 0x10
+
+    cmp cx, 0x0
+    jne print_16_hex_loop
 
     popa
     ret
 
+print_16_nl:
+    push ax
     
+    mov ah, 0x0e
+    mov al, 0x0a ; newline char
+    int 0x10
+    mov al, 0x0d ; carriage return
+    int 0x10
+    
+    pop ax
+    ret
+
 
--- test/test16BitPrint/test16BitPrint.asm
+++ test/test16BitPrint/test16BitPrint.asm
@@ -1,12 +1,21 @@
 [org 0x7c00]
 
-mov bp 0x9000
+mov bp, 0x9000       ; Init stack pointers
 mov sp, bp
 
-mov 
+mov bx, 0x78aa      ; Test print_hex
+call print_16_hex
+
+call print_16_nl    ; Test print_nl
+
+mov bx, HELLO_WORLD ; Test print_string
+call print_16_string
+
 
 jmp $
 
+HELLO_WORLD:
+    db "Hello world!", 0
 %include "../../master/16BitPrint/16BitPrint.asm"  ; print_16_string, print_16_hex, print_16_nl
 
 times 510 - ($ - $$) db 0x0
