ff86f75cd20e1f07530e5f16b078d15308e83e02
Leegenux <limian22@gmail.com>
2018-10-04 11:43:19 +0800

    update loadDiskTest

--- test/loadDiskTest/loadDiskTest.asm
+++ test/loadDiskTest/loadDiskTest.asm
@@ -1,12 +1,13 @@
-[bits 16]
+[org 0x7c00]
 
+[bits 16]
 ; Set up disk
 mov bp, 0x9000
 mov sp, bp
 
 mov [BOOT_DRIVE_NUMBER], dl
 
-mov bx, 0x0901      ; Load the disk
+mov bx, 0x0900     ; Load the disk
 mov es, bx
 mov bx, 0x0000
 mov al, 0x05
@@ -17,8 +18,11 @@ mov cl, 0x02
 
 call load_disk
 
-mov bx, 0x0901      ; Print the read data result
+mov bx, 0x0900      ; Print the read data result
 mov ds, bx
+mov bx, [0]
+call print_16_hex
+call print_16_nl
 mov bx, [512]
 call print_16_hex
 call print_16_nl
@@ -29,9 +33,6 @@ mov bx, [1536]
 call print_16_hex
 call print_16_nl
 mov bx, [2048]
-call print_16_hex
-call print_16_nl
-mov bx, [2560]
 call print_16_hex
 call print_16_nl
 
@@ -49,3 +50,7 @@ times 256 dw 0x2345
 times 256 dw 0x3456
 times 256 dw 0x4567
 times 256 dw 0xabcd
+
+; From this snippet of code, we know 2 things
+; 1. The block of memory that the base pointer points to stores nothing, so you can safely write dat onto it.
+; 2. 16bit segment can serve as page.
