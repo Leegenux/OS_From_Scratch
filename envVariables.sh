adb2ef89ceb5d0a3f193732b4da6eb7d38778524
Leegenux <limian22@gmail.com>
2018-10-14 15:22:26 +0800

    Add enVariables for the convenience of configuring the compilation environment.

---
+++ envVariables.sh
@@ -0,0 +1,8 @@
+export TARGET=i386-elf
+export LD=/usr/local/Cellar/gcc/8.2.0/bin/gcc-8
+export CC=/usr/local/Cellar/gcc/8.2.0/bin/gcc-8
+export PREFIX=/usr/local/i386elf
+export HOST=i386-pc-linux-gnu
+export CXX=/usr/local/Cellar/gcc/8.2.0/bin/g++-8
+export PATH="$PREFIX/bin:$PATH"  # Bins in this directory should not be messed up with it in other places
+export PROGRAM_PREFIX="i386-elf-"
